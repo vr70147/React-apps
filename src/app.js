89519584107c066b2c51dd2ffc79da95da5ed399
Raanan Adut <raanan@GI.LOCAL>
2019-11-17 15:59:33 +0200

    build components

--- src/app.js
+++ src/app.js
@@ -1,10 +1,72 @@
-console.log('ok')
+class IndescisionApp extends React.Component {
+    render() {
+        const title = 'Indecision';
+        const subtitle = 'Put your life in the hand of computer';
+        return (
+            <div>
+                <Header title={title} subtitle={subtitle} />
+                <Action />
+                <Options />
+                <AddOption />
+            </div>
+        )
+    }
+}
 
-const template = (
-    <div>
-        <h1>Hello</h1>
-    </div>
-)
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
+            </div>
+        )
+    }
+}
+
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should i do?</button>
+            </div>
+        )
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>This title from Options</p>
+                <Option />
+            </div>
+        )
+    }
+}
+
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Option One</p>
+                <p>Option Two</p>
+            </div>
+        )
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>Add Option</button>
+            </div>
+        )
+    }
+}
+
+
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(<IndescisionApp />, appRoot)
